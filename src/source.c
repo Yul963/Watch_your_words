5e2bee4e81958cb21084ecdf7bdab6ef43eb4c24
Yul963 <dbfrb9786@gmail.com>
2023-10-10 23:13:15 +0900

    sinewave generation

--- src/source.c
+++ src/source.c
@@ -1,114 +1,126 @@
 #include <obs-module.h>
 #include <obs.h>
 #include <math.h>
+#include <plugin-support.h>
 
 #include "whisper.h"
+
+#ifndef M_PI
+#define M_PI 3.1415926535897932384626433832795
+#endif
+
 #define do_log(level, format, ...)                 \
-	blog(level, "[gain filter: '%s'] " format, \
+	blog(level, "[word filter: '%s'] " format, \
 	     obs_source_get_name(gf->context), ##__VA_ARGS__)
 
 #define warn(format, ...) do_log(LOG_WARNING, format, ##__VA_ARGS__)
 #define info(format, ...) do_log(LOG_INFO, format, ##__VA_ARGS__)
 
-#define S_GAIN_DB "db"
+#define S_wyw_DB "db"
 
 #define MT_ obs_module_text
-#define TEXT_GAIN_DB MT_("Gain.GainDB")
+#define TEXT_wyw_DB MT_("wyw.wywDB")
 
  whisper_token_data data;//just test
 
-struct gain_data {
+struct wyw_data {
 	obs_source_t *context;
 	size_t channels;
 	float multiple;
 };
 
-static const char *gain_name(void *unused)
+static const char *wyw_name(void *unused)
 {
 	UNUSED_PARAMETER(unused);
-	return obs_module_text("Gain");
+	
+	return MT_("WatchYourWord");
 }
 
-static void gain_destroy(void *data)
+static void wyw_destroy(void *data)
 {
-	struct gain_data *gf = (struct gain_data *)data;
+	struct wyw_data *gf = (struct wyw_data *)data;
 	bfree(gf);
 }
 
-static void gain_update(void *data, obs_data_t *s)
+static void wyw_update(void *data, obs_data_t *s)
 {
-	struct gain_data *gf = (struct gain_data *)data;
-	double val = obs_data_get_double(s, S_GAIN_DB);
+	struct wyw_data *gf = (struct wyw_data *)data;
+	double val = obs_data_get_double(s, S_wyw_DB);
 	gf->channels = audio_output_get_channels(obs_get_audio());
 	//gf->multiple = db_to_mul((float)val);
 }
 
-static void *gain_create(obs_data_t *settings, obs_source_t *filter)
+static void *wyw_create(obs_data_t *settings, obs_source_t *filter)
 {
-	struct gain_data *gf = bzalloc(sizeof(*gf));
+	struct wyw_data *gf = bzalloc(sizeof(*gf));
 	gf->context = filter;
-	gain_update(gf, settings);
+	wyw_update(gf, settings);
 	return gf;
 }
 
-static struct obs_audio_data *gain_filter_audio(void *data,
+static double sin_val = 0.0;
+static struct obs_audio_data *wyw_filter_audio(void *data,
 						struct obs_audio_data *audio)
 {
-	struct gain_data *gf = (struct gain_data *)data;
+	double rate = 1000.0 / 48000.0;
+	//struct obs_source_audio data;
+	struct wyw_data *gf = (struct wyw_data *)data;
 	const size_t channels = gf->channels;
-	float **adata = (float **)audio->data;
-	const float multiple = gf->multiple;
+	//uint8_t **adata = audio->data;
+	float **fdata = (float **)audio->data;
+	float multiple = gf->multiple;
+	//uint64_t start = 1584359553172100, end = 1584359553172100;
+	//obs_log(LOG_INFO, "watch-your-words test timstamp: %llu)", audio->timestamp);
+	//if ((audio->timestamp > start) && (audio->timestamp < end)) {
+		for (size_t c = 0; c < channels; c++) {
+			if (audio->data[c]) {
+				//obs_log(LOG_INFO,"watch-your-words test audio_data: %u)",adata[c][0]);
+				for (size_t i = 0; i < audio->frames; i++) {
+					sin_val += rate * M_PI * 2.0;
+					if (sin_val > M_PI * 2.0)
+						sin_val -= M_PI * 2.0;
+					double wave = sin(sin_val);
+					fdata[c][i] =(float)(wave/100.0);
 
-	for (size_t c = 0; c < channels; c++) {
-		if (audio->data[c]) {
-			for (size_t i = 0; i < audio->frames; i++) {
-				adata[c][i] *= multiple;
+					//obs_log(LOG_INFO,"audio_data: %f, %d)",fdata[c][i], i);
+					/* multiple = adata[c][i];
+					adata[c][i] = adata[c][audio->frames - i - 1];
+					adata[c][audio->frames-i] = multiple;*/
+					//adata[c][i] = 0.009929f;
+				}
+				//obs_log(LOG_INFO,"watch-your-words test audio_data: %u)",adata[c][0]);
 			}
 		}
-	}
-
+	//}
 	return audio;
 }
 
-static void gain_defaults(obs_data_t *s)
+static void wyw_defaults(obs_data_t *s)
 {
-	obs_data_set_default_double(s, S_GAIN_DB, 0.0f);
+	obs_data_set_default_double(s, S_wyw_DB, 0.0f);
 }
 
-static obs_properties_t *gain_properties(void *data)
+static obs_properties_t *wyw_properties(void *data)
 {
 	obs_properties_t *ppts = obs_properties_create();
 
 	obs_property_t *p = obs_properties_add_float_slider(
-		ppts, S_GAIN_DB, TEXT_GAIN_DB, -30.0, 30.0, 0.1);
+		ppts, S_wyw_DB, TEXT_wyw_DB, -30.0, 30.0, 0.1);
 	obs_property_float_set_suffix(p, " dB");
 
 	UNUSED_PARAMETER(data);
 	return ppts;
 }
 
-struct obs_source_info gain_filter = {
-	.id = "gain_filter",
+struct obs_source_info watch_your_words = {
+	.id = "watch_your_words",
 	.type = OBS_SOURCE_TYPE_FILTER,
 	.output_flags = OBS_SOURCE_AUDIO,
-	.get_name = gain_name,
-	.create = gain_create,
-	.destroy = gain_destroy,
-	.update = gain_update,
-	.filter_audio = gain_filter_audio,
-	.get_defaults = gain_defaults,
-	.get_properties = gain_properties,
+	.get_name = wyw_name,
+	.create = wyw_create,
+	.destroy = wyw_destroy,
+	.update = wyw_update,
+	.filter_audio = wyw_filter_audio,
+	.get_defaults = wyw_defaults,
+	.get_properties = wyw_properties,
 };
-/*
-struct obs_source_info source = {
-	.id = "source",
-	.type = OBS_SOURCE_TYPE_INPUT,
-	.output_flags = OBS_SOURCE_VIDEO,
-	.get_name = source_name,
-	.create = source_create,
-	.destroy = source_destroy,
-	.update = source_update,
-	.video_render = source_render,
-	.get_width = source_width,
-	.get_height = source_height
-};*/
